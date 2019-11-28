5a309f6b16ae0b84ac8f380a7bac5e6eb8caf75c
Thaihamster <asadanaohisa8215@gmail.com>
2019-11-28 16:53:12 +0700

    change design for rooms show

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,9 +5,14 @@ class RoomsController < ApplicationController
     @rooms = Room.all.order(:id)
   end
 
+  def new
+    Room.create!
+    redirect_to rooms_path
+  end
+
   def show
     @room = Room.find(params[:id])
-    @messages = @room.messages
+    @messages = @room.messages.order(:id)
   end
 
 end
