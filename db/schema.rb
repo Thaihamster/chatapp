87efff3b7942c37280ad661dfcf4784d6d225f89
Thaihamster <asadanaohisa8215@gmail.com>
2019-11-29 09:31:57 +0700

    change order of mig files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191128021537) do
+ActiveRecord::Schema.define(version: 20191129022745) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
